f9b3a2a54fa31dda6dd175cbf92e848716464e3c
league <league@imac-109.attlocal.net>
2022-04-22 16:49:51 -0700

    Walkoffame WalkOfFame

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,8 +9,84 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		
+rob.setX(0);
+rob.setY(250);
+rob.penDown();
+rob.setSpeed(50);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.setAngle(90);
+rob.setX(50);
+rob.setY(250);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.setAngle(90);
+rob.setX(100);
+rob.setY(250);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.setAngle(90);
+rob.setX(150);
+rob.setY(250);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.setAngle(90);
+rob.setX(200);
+rob.setY(250);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.turn(144);
+rob.move(30);
+rob.setAngle(90);
+rob.setX(250);
+rob.setY(250);
+
 
 		// 3. Set the length of each line in the star to 30.
 
